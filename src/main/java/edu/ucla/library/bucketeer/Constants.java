6d8464441e07a4f20649a8e0ce81124d8dabd6dc
Kevin S. Clarke <ksclarke@ksclarke.io>
2019-03-19 23:21:58 -0400

    Update config options

--- src/main/java/edu/ucla/library/bucketeer/Constants.java
+++ src/main/java/edu/ucla/library/bucketeer/Constants.java
@@ -9,6 +9,19 @@ public final class Constants {
     // The default port at which to run the application
     public static final int DEFAULT_PORT = 8888;
 
+    //
+    // The properties below can be set through the configuration file
+    //
+
+    // The HTTP port configuration parameter
+    public static final String HTTP_PORT = "http.port";
+
+    // The OpenAPI specification configuration parameter
+    public static final String OPENAPI_SPEC_PATH = "openapi.spec.path";
+
+    /**
+     * Private constructor for the Constants class.
+     */
     private Constants() {
     }
 
--- src/main/java/edu/ucla/library/bucketeer/MainVerticle.java
+++ src/main/java/edu/ucla/library/bucketeer/MainVerticle.java
@@ -12,7 +12,7 @@ import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
  */
 public class MainVerticle extends AbstractVerticle {
 
-    private static final String API_SPEC = "src/main/resources/bucketeer.yaml";
+    private static final String DEFAULT_SPEC_PATH = "src/main/resources/bucketeer.yaml";
 
     /**
      * Starts a Web server.
@@ -21,11 +21,12 @@ public class MainVerticle extends AbstractVerticle {
     @SuppressWarnings("rawtypes")
     public void start(final Future<Void> aFuture) {
         final HttpServer server = vertx.createHttpServer();
+        final String apiSpec = config().getString(Constants.OPENAPI_SPEC_PATH, DEFAULT_SPEC_PATH);
 
-        OpenAPI3RouterFactory.create(vertx, API_SPEC, creation -> {
+        OpenAPI3RouterFactory.create(vertx, apiSpec, creation -> {
             if (creation.succeeded()) {
                 final OpenAPI3RouterFactory routerFactory = creation.result();
-                final int port = config().getInteger("http.port", Constants.DEFAULT_PORT);
+                final int port = config().getInteger(Constants.HTTP_PORT, Constants.DEFAULT_PORT);
 
                 routerFactory.addHandlerByOperationId(Op.GET_PING, new GetPingHandler());
                 server.requestHandler(routerFactory.getRouter()).listen(port);
