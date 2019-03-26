a909ddb0060edb68a85c22911f2f66f19420c84c
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-03-26 15:12:01 -0500

    [IIIF-105] Add LoadImage handler endpoint

--- src/main/java/edu/ucla/library/bucketeer/Op.java
+++ src/main/java/edu/ucla/library/bucketeer/Op.java
@@ -4,6 +4,7 @@ package edu.ucla.library.bucketeer;
 public final class Op {
 
     public static final String GET_PING = "getPing";
+    public static final String LOAD_IMAGE = "loadImage";
 
     private Op() {
     }
---
+++ src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
@@ -0,0 +1,25 @@
+
+package edu.ucla.library.bucketeer.handlers;
+
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.api.RequestParameter;
+import io.vertx.ext.web.api.RequestParameters;
+
+public class LoadImageHandler implements Handler<RoutingContext> {
+
+
+    @Override
+    public void handle(final RoutingContext aContext) {
+        final HttpServerResponse response = aContext.response();
+
+        final RequestParameters params = aContext.get("parsedParameters");
+        final RequestParameter imageId = params.queryParameter("imageId");
+        final RequestParameter filePath = params.queryParameter("filePath");
+
+        response.setStatusCode(200);
+        response.putHeader("content-type", "text/plain").end("Hello");
+    }
+
+}
