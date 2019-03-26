7e3b70ff15909d0d330f563ad57be86d2b2981a6
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-03-26 17:27:28 -0500

    [IIIF-105] use a JSON object for response, as per the spec

--- src/main/java/edu/ucla/library/bucketeer/Op.java
+++ src/main/java/edu/ucla/library/bucketeer/Op.java
@@ -5,6 +5,8 @@ public final class Op {
 
     public static final String GET_PING = "getPing";
     public static final String LOAD_IMAGE = "loadImage";
+    public static final String IMAGE_ID = "imageId";
+    public static final String FILE_PATH = "filePath";
 
     private Op() {
     }
--- src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
+++ src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
@@ -1,8 +1,10 @@
 
 package edu.ucla.library.bucketeer.handlers;
 
+import edu.ucla.library.bucketeer.Op;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.api.RequestParameter;
 import io.vertx.ext.web.api.RequestParameters;
@@ -15,13 +17,13 @@ public class LoadImageHandler implements Handler<RoutingContext> {
         final HttpServerResponse response = aContext.response();
 
         final RequestParameters params = aContext.get("parsedParameters");
-        final RequestParameter imageId = params.queryParameter("imageId");
-        final RequestParameter filePath = params.queryParameter("filePath");
+        final RequestParameter imageId = params.queryParameter(Op.IMAGE_ID);
+        final RequestParameter filePath = params.queryParameter(Op.FILE_PATH);
 
+        final JsonObject json = new JsonObject().put(Op.IMAGE_ID, imageId);
+        json.put(Op.FILE_PATH, filePath);
         response.setStatusCode(200);
-        response.putHeader("imageId", imageId.toString());
-        response.putHeader("filePath", filePath.toString());
-        response.putHeader("content-type", "text/plain").end("SUCCESS!");
+        response.putHeader("content-type", "application/json").end(json.toBuffer());
     }
 
 }
