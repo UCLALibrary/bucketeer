098c2b63ed046c719985a5276a5ea0ecf836604c
Kevin S. Clarke <ksclarke@ksclarke.io>
2019-03-28 21:46:28 -0400

    Update logging default
    * Adjust logging defaults
    * Clean Op vs. Constants

---
+++ src/main/java/edu/ucla/library/bucketeer/Constants.java
@@ -0,0 +1,34 @@
+
+package edu.ucla.library.bucketeer;
+
+/**
+ * A class for package constants.
+ */
+public final class Constants {
+
+    /**
+     * ResourceBundle file name for I18n messages.
+     */
+    public static final String MESSAGES = "bucketeer_messages";
+
+    /**
+     * Callback URL
+     */
+    public static final String CALLBACK_URL = "callback_url";
+
+    /**
+     * The ID for the image that's going to be processed.
+     */
+    public static final String IMAGE_ID = "imageId";
+
+    /**
+     * The path to the TIFF file that's going to be read.
+     */
+    public static final String FILE_PATH = "filePath";
+
+    /**
+     * Private constructor for Constants class.
+     */
+    private Constants() {
+    }
+}
--- src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
+++ src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
@@ -1,7 +1,11 @@
 
 package edu.ucla.library.bucketeer.handlers;
 
-import edu.ucla.library.bucketeer.Op;
+import info.freelibrary.util.Logger;
+import info.freelibrary.util.LoggerFactory;
+
+import edu.ucla.library.bucketeer.Constants;
+import edu.ucla.library.bucketeer.MessageCodes;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
@@ -11,17 +15,20 @@ import io.vertx.ext.web.api.RequestParameters;
 
 public class LoadImageHandler implements Handler<RoutingContext> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoadImageHandler.class, Constants.MESSAGES);
 
     @Override
     public void handle(final RoutingContext aContext) {
         final HttpServerResponse response = aContext.response();
 
         final RequestParameters params = aContext.get("parsedParameters");
-        final RequestParameter imageId = params.queryParameter(Op.IMAGE_ID);
-        final RequestParameter filePath = params.queryParameter(Op.FILE_PATH);
+        final RequestParameter imageId = params.queryParameter(Constants.IMAGE_ID);
+        final RequestParameter filePath = params.queryParameter(Constants.FILE_PATH);
 
-        final JsonObject json = new JsonObject().put(Op.IMAGE_ID, imageId);
-        json.put(Op.FILE_PATH, filePath);
+        LOGGER.debug(MessageCodes.BUCKETEER_000, "imageID is null: " + (imageId == null));
+
+        final JsonObject json = new JsonObject().put(Constants.IMAGE_ID, imageId);
+        json.put(Constants.FILE_PATH, filePath);
         response.setStatusCode(200);
         response.putHeader("content-type", "application/json").end(json.toBuffer());
     }
