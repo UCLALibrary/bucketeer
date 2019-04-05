72a8811594901de57be1bb750ec4c763d5079d85
Hardy Pottinger <hpottinger@library.ucla.edu>
2019-04-05 15:01:49 -0500

    WIP

--- src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
+++ src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
@@ -1,33 +1,31 @@
 
 package edu.ucla.library.bucketeer.handlers;
 
-import info.freelibrary.util.Logger;
-import info.freelibrary.util.LoggerFactory;
+//import info.freelibrary.util.Logger;
+//import info.freelibrary.util.LoggerFactory;
 
 import edu.ucla.library.bucketeer.Constants;
-import edu.ucla.library.bucketeer.MessageCodes;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.api.RequestParameter;
-import io.vertx.ext.web.api.RequestParameters;
 import org.codehaus.plexus.util.StringUtils;
 
 public class LoadImageHandler implements Handler<RoutingContext> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoadImageHandler.class, Constants.MESSAGES);
+    /* saving logging for later
+     * private static final Logger LOGGER = LoggerFactory.getLogger(LoadImageHandler.class, Constants.MESSAGES);
+     */
 
     @Override
     public void handle(final RoutingContext aContext) {
         final HttpServerResponse response = aContext.response();
 
-        final RequestParameters params = aContext.get("parsedParameters");
-        final RequestParameter imageId = params.queryParameter(Constants.IMAGE_ID);
-        final RequestParameter filePath = params.queryParameter(Constants.FILE_PATH);
+        final String imageId = aContext.pathParams().get(Constants.IMAGE_ID);
+        final String filePath = aContext.pathParams().get(Constants.FILE_PATH);
 
         /* handle common error conditions */
-        if (StringUtils.isBlank(imageId.toString()) || StringUtils.isBlank(filePath.toString()) ) {
+        if (StringUtils.isBlank(imageId) || StringUtils.isBlank(filePath) ) {
             response.setStatusCode(400);
             response.putHeader(Constants.CONTENT_TYPE, "text/plain").end("400 Bad Request: imageId/filePath required");
         }
