b5a4793beb80c4b63fdd11714e478e9ef842a873
Hardy Pottinger <hpottinger@library.ucla.edu>
2019-04-26 16:35:30 -0500

    [IIIF-166] WIP, probably won't compile, refactoring to test S3 bucket, I'm a mess

--- src/test/java/edu/ucla/library/bucketeer/imageUploadIT.java
+++ src/test/java/edu/ucla/library/bucketeer/imageUploadIT.java
@@ -12,9 +12,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
 import static io.restassured.RestAssured.get;
 import info.freelibrary.util.Logger;
 import info.freelibrary.util.LoggerFactory;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 /**
  * This test confirms that our entire imageUpload process works as expected.
@@ -30,6 +37,14 @@ public class imageUploadIT {
     private File myTIFF;
     private String myUUID;
     private String myImageLoadRequest;
+    private AmazonS3 amazonS3;
+    private static final String DEFAULT_ACCESS_KEY = "YOUR_ACCESS_KEY";
+    private static final String DEFAULT_S3_BUCKET = "cantaloupe-jp2k";
+    private String myS3Bucket;
+    
+    /** We can't, as of yet, execute these tests without a non-default S3 configuration */
+    private boolean isExecutable;
+
     /**
      * Use RestAssured to connect to our service
      */
@@ -55,6 +70,23 @@ public class imageUploadIT {
     @Test
     public final void checkThatWeCanLoadAnImage() throws UnsupportedEncodingException {
         
+        final Vertx vertx;
+        final ConfigRetriever configRetriever = ConfigRetriever.create(vertx);
+        
+        
+        
+        configRetriever.getConfig(config -> {
+            if (config.succeeded()) {
+                final JsonObject jsonConfig = config.result();
+                myS3Bucket = jsonConfig.getString(Config.S3_BUCKET, DEFAULT_S3_BUCKET);
+
+                // We need to determine if we'll be able to run the S3 integration tests so we can skip if needed
+                if (jsonConfig.containsKey(Config.S3_ACCESS_KEY) && !jsonConfig.getString(Config.S3_ACCESS_KEY,
+                        DEFAULT_ACCESS_KEY).equalsIgnoreCase(DEFAULT_ACCESS_KEY)) {
+                    isExecutable = true;
+                }
+            }
+        
         // let's use our test.if file
         myTIFF = new File("src/test/resources/images/test.tif");
         // and we'll pick a random ID for it
@@ -73,6 +105,8 @@ public class imageUploadIT {
         // we should probably wait a bit for things to happen
 
         // then we should check the S3 bucket to which we are sending JP2s
+        
+        assertTrue(amazonS3.doesObjectExist(bucketName, defaultTestFileName ));
         
     }
 }
