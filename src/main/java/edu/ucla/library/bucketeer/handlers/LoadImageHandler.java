4eab092fe7f1c3b10bd3f02e497e20d4b12001df
Kevin S. Clarke <ksclarke@library.ucla.edu>
2019-04-26 13:41:53 -0400

    Minor cleanup

--- src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
+++ src/main/java/edu/ucla/library/bucketeer/handlers/LoadImageHandler.java
@@ -8,18 +8,20 @@ import info.freelibrary.util.StringUtils;
 import edu.ucla.library.bucketeer.Constants;
 import edu.ucla.library.bucketeer.HTTP;
 import edu.ucla.library.bucketeer.MessageCodes;
+import edu.ucla.library.bucketeer.verticles.ImageWorkerVerticle;
 import io.vertx.core.Handler;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.eventbus.DeliveryOptions;
-import io.vertx.core.eventbus.EventBus;
 import io.vertx.ext.web.RoutingContext;
 
 public class LoadImageHandler implements Handler<RoutingContext> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoadImageHandler.class, Constants.MESSAGES);
-    private static final String IMAGEWORKERVERTICLENAME = "edu.ucla.library.bucketeer.verticles.ImageWorkerVerticle";
+
+    private static final String IMAGE_WORKER_VERTICLE = ImageWorkerVerticle.class.getName();
 
     @Override
     public void handle(final RoutingContext aContext) {
@@ -37,36 +39,34 @@ public class LoadImageHandler implements Handler<RoutingContext> {
             response.putHeader(Constants.CONTENT_TYPE, "text/plain").end(responseMessage);
             response.close();
         } else {
-          // OK, we have a complete imageLoad request... let's do something with it.
-          // We do this by passing a message to the ImageWorker Verticle
-          // the message ImageWorker expects is a json object, with the following
-          // parameters: filePath, imageId
+            // OK, we have a complete imageLoad request... let's do something with it.
+            // We do this by passing a message to the ImageWorker Verticle
+            // the message ImageWorker expects is a JSON object, with the following
+            // parameters: filePath, imageId
 
             try {
                 final JsonObject imageWorkJson = new JsonObject();
+                final DeliveryOptions options = new DeliveryOptions();
+
                 imageWorkJson.put(Constants.IMAGE_ID, imageId);
                 imageWorkJson.put(Constants.FILE_PATH, filePath);
-                final DeliveryOptions options = new DeliveryOptions();
-                //FIXME: this should be in a utility method somewhere
-                // send a message to the imageworker verticle that we have work for it to do
-                eventBus.send(IMAGEWORKERVERTICLENAME, imageWorkJson, options, workerResponse -> {
+
+                // FIXME: this should be in a utility method somewhere
+                // send a message to the image worker verticle that we have work for it to do
+                eventBus.send(IMAGE_WORKER_VERTICLE, imageWorkJson, options, workerResponse -> {
                     if (workerResponse.failed()) {
                         if (workerResponse.cause() != null) {
-                            LOGGER.error(workerResponse.cause(), MessageCodes.BUCKETEER_005,
-                                    IMAGEWORKERVERTICLENAME, imageWorkJson);
-                        } else {
-                            LOGGER.error(MessageCodes.BUCKETEER_005, IMAGEWORKERVERTICLENAME,
+                            LOGGER.error(workerResponse.cause(), MessageCodes.BUCKETEER_005, IMAGE_WORKER_VERTICLE,
                                     imageWorkJson);
+                        } else {
+                            LOGGER.error(MessageCodes.BUCKETEER_005, IMAGE_WORKER_VERTICLE, imageWorkJson);
                         }
                     }
                 });
 
-
-
             } catch (final Exception details) {
                 LOGGER.error(details, MessageCodes.BUCKETEER_023, details.getMessage());
             }
-
 
             final JsonObject responseJson = new JsonObject();
 
