958313979762171973b2bfd946d043c7bc4b5637
Hardy Pottinger <hpottinger@library.ucla.edu>
2019-10-02 16:02:36 -0500

    [IIIF-472] Remove any at symbols @ from provided handles (#73)
    * [IIIF-472] drop at symbol from all slack handles before adding it back
    
    * [IIIF-472] filter out at symbols at the form handler, too
    
    * [IIIF-472] requested changes

--- src/main/java/edu/ucla/library/bucketeer/handlers/LoadCsvHandler.java
+++ src/main/java/edu/ucla/library/bucketeer/handlers/LoadCsvHandler.java
@@ -118,13 +118,14 @@ public class LoadCsvHandler implements Handler<RoutingContext> {
             response.putHeader(Constants.CONTENT_TYPE, Constants.HTML);
             response.end(StringUtils.format(myExceptionPage, errorMessage + extendedErrorMessage));
         } else {
+            final String cleanSlackHandle = slackHandle.replace(Constants.AT, Constants.EMPTY_STRING);
             final String runFailures = StringUtils.trimTo(formAttributes.get(Constants.FAILURES_ONLY), "false");
             final FileUpload csvFile = csvUploads.iterator().next();
             final String filePath = csvFile.uploadedFileName();
             final String fileName = csvFile.fileName();
             final String jobName = FileUtils.stripExt(fileName);
 
-            LOGGER.info(MessageCodes.BUCKETEER_051, slackHandle, fileName, filePath);
+            LOGGER.info(MessageCodes.BUCKETEER_051, cleanSlackHandle, fileName, filePath);
 
             if (myVertx == null) {
                 myVertx = aContext.vertx();
@@ -134,7 +135,7 @@ public class LoadCsvHandler implements Handler<RoutingContext> {
                 final Job job = JobFactory.getInstance().createJob(jobName, new File(filePath));
 
                 // Set the Slack handle and whether the job is a subsequent or initial run
-                job.setSlackHandle(slackHandle).setIsSubsequentRun(Boolean.parseBoolean(runFailures));
+                job.setSlackHandle(cleanSlackHandle).setIsSubsequentRun(Boolean.parseBoolean(runFailures));
 
                 initiateJob(job, response);
             } catch (final FileNotFoundException details) {
