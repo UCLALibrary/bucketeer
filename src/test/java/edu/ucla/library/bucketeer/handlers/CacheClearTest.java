2eed66b54e337b3d2338664e99cb668db2574462
angelahuqing <angelahuqing@gmail.com>
2022-05-31 09:44:33 -0700

    Iiif 1142 slight return 1 (#139)
    * [WIP] Work in progress checkpoint
    
    * Fix for FilesystemWriteCsvFfOnT test failure
    
    * Bump failsafe and docker-maven-plugin versions
    
    * Update BatchJobStatusHandler.java
    
    * Update pom.xml
    
    * Broken out test class
    
    * Add a few more notes
    
    Co-authored-by: Kevin S. Clarke <ksclarke@ksclarke.io>

---
+++ src/test/java/edu/ucla/library/bucketeer/handlers/CacheClearTest.java
@@ -0,0 +1,105 @@
+
+package edu.ucla.library.bucketeer.handlers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import info.freelibrary.util.Logger;
+import info.freelibrary.util.LoggerFactory;
+import info.freelibrary.util.StringUtils;
+
+import edu.ucla.library.bucketeer.Config;
+import edu.ucla.library.bucketeer.Constants;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.WebClient;
+
+/**
+ * Testing the mechanism used to clear Cantaloupe's cache.
+ */
+@RunWith(VertxUnitRunner.class)
+public class CacheClearTest {
+
+    static {
+        // For running this test independently, we need to configure the logging config source
+        System.setProperty("logback.configurationFile", "src/test/resources/logback-test.xml");
+    }
+
+    /**
+     * Logger for the <code>CacheClearTest</code>.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(CacheClearTest.class, Constants.MESSAGES);
+
+    /**
+     * Cantaloupe username for testing Cantaloupe cache clearing.
+     */
+    private String myUsername;
+
+    /**
+     * Cantaloupe password for testing Cantaloupe cache clearing.
+     */
+    private String myPassword;
+
+    /**
+     * Sets up the tests.
+     *
+     * @param aContext A testing environment
+     */
+    @Before
+    public final void setup(final TestContext aContext) {
+        final ConfigRetriever configRetriever = ConfigRetriever.create(Vertx.vertx());
+        final Async asyncTask = aContext.async();
+
+        configRetriever.getConfig(configuration -> {
+            if (configuration.failed()) {
+                aContext.fail(configuration.cause());
+            } else {
+                final JsonObject config = configuration.result();
+
+                // Gives a way to pull these from the test's full runtime environment
+                myUsername = config.getString(Config.IIIF_CACHE_USER);
+                myPassword = config.getString(Config.IIIF_CACHE_PASSWORD);
+
+                // Gives a way to supply these manually at test time in the code; just don't check values into VC!
+                if (myUsername == null || myPassword == null) {
+                    myUsername = "";
+                    myPassword = "";
+                }
+
+                asyncTask.complete();
+            }
+        });
+    }
+
+    /**
+     * Tests clearing Cantaloupe's cache.
+     *
+     * @param aContext A testing context
+     */
+    @Test
+    public void testCacheClear(final TestContext aContext) {
+        final WebClient client = WebClient.create(Vertx.vertx());
+        final Async asyncTask = aContext.async();
+
+        // These are just the property names, not values
+        LOGGER.info("Connecting with Cantaloupe user: {}", StringUtils.trimToNull(myUsername));
+
+        client.postAbs("https://test-iiif.library.ucla.edu/tasks").basicAuthentication(myUsername, myPassword)
+                .putHeader("content-type", "application/json")
+                .sendJsonObject(new JsonObject().put("verb", "PurgeItemFromCache").put("identifier", "blah"), post -> {
+                    if (post.succeeded()) {
+                        LOGGER.info(Integer.toString(post.result().statusCode()));
+                    } else {
+                        LOGGER.error(post.cause(), post.cause().getMessage());
+                    }
+
+                    asyncTask.complete();
+                });
+    }
+}
